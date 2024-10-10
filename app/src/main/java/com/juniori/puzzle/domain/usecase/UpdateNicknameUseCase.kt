beaeb0fb832bc9ac771b9c43cddfccab8abcc6b3
주재완 <yjjw2000@naver.com>
2024-10-10 17:22:57 +0900

    Revert "refactor : UpdateNickname에서 API 통신할 때 withcontext로 감싸져 있던 부분 수정"
    This reverts commit 5650a7a8e3e2542c7427e59d99576f13db6fcb34.

--- app/src/main/java/com/juniori/puzzle/domain/usecase/UpdateNicknameUseCase.kt
+++ app/src/main/java/com/juniori/puzzle/domain/usecase/UpdateNicknameUseCase.kt
@@ -1,10 +1,13 @@
 package com.juniori.puzzle.domain.usecase
 
+import com.juniori.puzzle.data.APIResponse
 import com.juniori.puzzle.domain.APIErrorType
 import com.juniori.puzzle.domain.TempAPIResponse
 import com.juniori.puzzle.domain.entity.UserInfoEntity
 import com.juniori.puzzle.domain.repository.AuthRepository
 import com.juniori.puzzle.domain.repository.VideoRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UpdateNicknameUseCase @Inject constructor(
@@ -12,7 +15,9 @@ class UpdateNicknameUseCase @Inject constructor(
     private val videoRepository: VideoRepository
 ){
     suspend operator fun invoke(newNickname: String): TempAPIResponse<UserInfoEntity> {
-        val newInfo = authRepository.updateNickname(newNickname)
+        val newInfo = withContext(Dispatchers.IO) {
+            authRepository.updateNickname(newNickname)
+        }
 
         return if (newInfo is TempAPIResponse.Success) {
             videoRepository.updateServerNickname(newInfo.data)
