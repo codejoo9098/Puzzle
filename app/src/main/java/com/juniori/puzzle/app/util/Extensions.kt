5a87dfea46c0327bc749de7fcb065cc110a52056
주재완 <yjjw2000@naver.com>
2024-10-10 17:23:20 +0900

    Revert "refactor : Extensions 파일명 수정 및 파일명에 맞게 메소드 이동"
    This reverts commit 615af6e57d69217bbe3f8d70f980104fe799859d.

--- app/src/main/java/com/juniori/puzzle/app/util/Extensions.kt
+++ app/src/main/java/com/juniori/puzzle/app/util/Extensions.kt
@@ -2,11 +2,26 @@ package com.juniori.puzzle.app.util
 
 import android.annotation.SuppressLint
 import android.location.Address
+import com.google.android.gms.tasks.Task
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.suspendCancellableCoroutine
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.coroutines.resumeWithException
 
 @SuppressLint("SimpleDateFormat")
 private val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+@OptIn(ExperimentalCoroutinesApi::class)
+suspend fun <T> Task<T>.await(): T {
+    return suspendCancellableCoroutine { cont ->
+        addOnCompleteListener {
+            it.exception?.let { exception ->
+                cont.resumeWithException(exception)
+            } ?: cont.resume(it.result, null)
+        }
+    }
+}
 
 fun Address.toAddressString(): String {
     val list= mutableListOf<String>()
