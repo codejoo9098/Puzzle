84e1989b294a5153f873e9781d90ad83e149d139
주재완 <yjjw2000@naver.com>
2024-10-18 16:50:42 +0900

    test : Login 테스트 수정 및 로직 수정

--- app/src/test/java/com/juniori/puzzle/ui/login/LoginViewModelTest.kt
+++ app/src/test/java/com/juniori/puzzle/ui/login/LoginViewModelTest.kt
@@ -2,11 +2,14 @@ package com.juniori.puzzle.ui.login
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.juniori.puzzle.data.APIResponse
+import com.juniori.puzzle.domain.APIErrorType
+import com.juniori.puzzle.domain.TempAPIResponse
 import com.juniori.puzzle.domain.entity.UserInfoEntity
 import com.juniori.puzzle.domain.usecase.common.GetUserInfoUseCase
 import com.juniori.puzzle.domain.usecase.PostUserInfoUseCase
 import com.juniori.puzzle.domain.usecase.RequestLoginUseCase
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.After
@@ -17,9 +20,9 @@ import org.junit.Test
 import org.mockito.Mockito
 
 class LoginViewModelTest {
+    private val mockAccount = "MockAccount"
     lateinit var loginViewModel: LoginViewModel
 
-    lateinit var mockAccount: GoogleSignInAccount
     lateinit var mockUserInfoEntity: UserInfoEntity
 
     lateinit var mockGetUserInfoUseCase: GetUserInfoUseCase
@@ -33,7 +36,6 @@ class LoginViewModelTest {
     @Before
     fun setup() {
         Dispatchers.setMain(mainThreadSurrogate)
-        mockAccount = Mockito.mock(GoogleSignInAccount::class.java)
     }
 
     @Test
@@ -43,14 +45,14 @@ class LoginViewModelTest {
 
             setupUseCase()
 
-            Mockito.`when`(mockGetUserInfoUseCase()).thenReturn(APIResponse.Failure(Exception()))
-            Mockito.`when`(mockRequestLoginUseCase(mockAccount)).thenReturn(APIResponse.Success(mockUserInfoEntity))
+            Mockito.`when`(mockGetUserInfoUseCase()).thenReturn(MutableStateFlow(TempAPIResponse.Failure(APIErrorType.NO_CONTENT)))
+            Mockito.`when`(mockRequestLoginUseCase(mockAccount)).thenReturn(TempAPIResponse.Success(mockUserInfoEntity))
             Mockito.`when`(mockPostUserInfoUseCase(mockUserInfoEntity.uid, mockUserInfoEntity.nickname, mockUserInfoEntity.profileImage)).thenReturn(APIResponse.Success(mockUserInfoEntity))
 
             loginViewModel = LoginViewModel(mockGetUserInfoUseCase, mockRequestLoginUseCase, mockPostUserInfoUseCase)
             loginViewModel.loginUser(mockAccount).join()
 
-            assertEquals(APIResponse.Success(mockUserInfoEntity), loginViewModel.loginFlow.value)
+            assertEquals(TempAPIResponse.Success(mockUserInfoEntity), loginViewModel.loginFlow.value)
         }
     }
 
@@ -60,14 +62,14 @@ class LoginViewModelTest {
 
         setupUseCase()
 
-        Mockito.`when`(mockGetUserInfoUseCase()).thenReturn(APIResponse.Failure(Exception()))
-        Mockito.`when`(mockRequestLoginUseCase(mockAccount)).thenReturn(APIResponse.Failure(Exception()))
+        Mockito.`when`(mockGetUserInfoUseCase()).thenReturn(MutableStateFlow(TempAPIResponse.Failure(APIErrorType.NO_CONTENT)))
+        Mockito.`when`(mockRequestLoginUseCase(mockAccount)).thenReturn(TempAPIResponse.Failure(APIErrorType.SERVER_ERROR))
         Mockito.`when`(mockPostUserInfoUseCase(mockUserInfoEntity.uid, mockUserInfoEntity.nickname, mockUserInfoEntity.profileImage)).thenReturn(APIResponse.Success(mockUserInfoEntity))
 
         loginViewModel = LoginViewModel(mockGetUserInfoUseCase, mockRequestLoginUseCase, mockPostUserInfoUseCase)
         loginViewModel.loginUser(mockAccount).join()
 
-        assertTrue(loginViewModel.loginFlow.value is APIResponse.Failure)
+        assertTrue(loginViewModel.loginFlow.value is TempAPIResponse.Failure)
     }
 
     @Test
@@ -76,14 +78,14 @@ class LoginViewModelTest {
 
         setupUseCase()
 
-        Mockito.`when`(mockGetUserInfoUseCase()).thenReturn(APIResponse.Failure(Exception()))
-        Mockito.`when`(mockRequestLoginUseCase(mockAccount)).thenReturn(APIResponse.Success(mockUserInfoEntity))
+        Mockito.`when`(mockGetUserInfoUseCase()).thenReturn(MutableStateFlow(TempAPIResponse.Failure(APIErrorType.NO_CONTENT)))
+        Mockito.`when`(mockRequestLoginUseCase(mockAccount)).thenReturn(TempAPIResponse.Success(mockUserInfoEntity))
         Mockito.`when`(mockPostUserInfoUseCase(mockUserInfoEntity.uid, mockUserInfoEntity.nickname, mockUserInfoEntity.profileImage)).thenReturn(APIResponse.Failure(Exception()))
 
         loginViewModel = LoginViewModel(mockGetUserInfoUseCase, mockRequestLoginUseCase, mockPostUserInfoUseCase)
         loginViewModel.loginUser(mockAccount).join()
 
-        assertTrue(loginViewModel.loginFlow.value is APIResponse.Failure)
+        assertTrue(loginViewModel.loginFlow.value is TempAPIResponse.Failure)
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
